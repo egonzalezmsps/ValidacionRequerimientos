fa0ae5acdab0877ba40ae2974288b203d030dfb7
jesus enoc <egonzalezm@spsolutions.com.mx>
2026-05-06 16:10:55 -0600

    new config in the Verb Get/Apis

--- src/main/java/com/coppel/sla/controllers/ApiController.java
+++ src/main/java/com/coppel/sla/controllers/ApiController.java
@@ -2,18 +2,17 @@ package com.coppel.sla.controllers;
 
 import com.coppel.sla.dto.ApiResponseDto;
 import com.coppel.sla.services.ApiCatalogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/apis")
 public class ApiController {
 
     private final ApiCatalogService service;
 
-    public ApiController(ApiCatalogService service) {
-        this.service = service;
-    }
 
     @GetMapping
     public List<ApiResponseDto> getApis() {
--- src/main/java/com/coppel/sla/services/impl/ApiCatalogServiceImpl.java
+++ src/main/java/com/coppel/sla/services/impl/ApiCatalogServiceImpl.java
@@ -2,15 +2,14 @@ package com.coppel.sla.services.impl;
 
 import com.coppel.sla.component.DatadogClient;
 import com.coppel.sla.dto.ApiResponseDto;
-import com.coppel.sla.entities.ApiConfigEntity;
 import com.coppel.sla.mappers.ApiMapper;
 import com.coppel.sla.repositories.ApiConfigRepository;
 import com.coppel.sla.services.ApiCatalogService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import java.time.Instant;
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class ApiCatalogServiceImpl implements ApiCatalogService {
 
@@ -18,38 +17,14 @@ public class ApiCatalogServiceImpl implements ApiCatalogService {
     private final ApiConfigRepository repository;
     private final ApiMapper mapper;
 
-    public ApiCatalogServiceImpl(
-            DatadogClient datadogClient,
-            ApiConfigRepository repository,
-            ApiMapper mapper) {
-        this.datadogClient = datadogClient;
-        this.repository = repository;
-        this.mapper = mapper;
-    }
 
     @Override
-    @Transactional
     public List<ApiResponseDto> getApis() {
-        Instant now = Instant.now();
-        List<ApiConfigEntity> apis = datadogClient.listApis()
+
+        return datadogClient.listApis()
                 .stream()
-                .map(api -> enrich(api, now))
-                .toList();
-        repository.saveAll(apis);
-        return apis.stream()
                 .map(mapper::toDto)
                 .toList();
     }
 
-    private ApiConfigEntity enrich(ApiConfigEntity api, Instant now) {
-        api.setUpdatedAt(now);
-        api.setUpdatedBy("system");
-
-        if (api.getCreatedAt() == null) {
-            api.setCreatedAt(now);
-            api.setCreatedBy("system");
-        }
-
-        return api;
-    }
 }
